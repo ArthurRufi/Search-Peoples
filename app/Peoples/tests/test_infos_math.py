1dd7146cd6cbdcc52725197d7ffdaa690516261e
ArthurRufi <temparthur87@gmail.com>
2025-08-26 19:39:04 -0300

    testes criados completos para teste de calculo

--- app/Peoples/tests/test_infos_math.py
+++ app/Peoples/tests/test_infos_math.py
@@ -4,3 +4,11 @@ from app.Peoples.domain.contracts.infosmath import DeltaInfo
 def test_delta_calculation_social_returns_fail_negative_one():
     Infos = DeltaInfo(5)
     assert Infos.delta_social() == -1.0
+
+def test_delta_calc_social_returns_number_correctly():
+    Infos = DeltaInfo(2)
+    assert Infos.delta_social() == 0.5
+
+def test_delta_calc_social_returns_number_correctly_lower_bound():
+    Infos = DeltaInfo(1)
+    assert Infos.delta_social() == 0.25
