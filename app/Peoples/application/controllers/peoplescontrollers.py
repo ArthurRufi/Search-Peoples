7ac741f7a410e9ad516c68e4d224572e6c4e4d7a
ArthurRufi <temparthur87@gmail.com>
2025-08-28 21:23:58 -0300

    this is oh shit

--- app/Peoples/application/controllers/peoplescontrollers.py
+++ app/Peoples/application/controllers/peoplescontrollers.py
@@ -10,4 +10,9 @@ async def get_tax(id: int):
     
     return {"message": "Peoples route"}
 
+@routerpeoples.get("/vizualizer/")
+#adicoonar o recebimento para a rota
+async def view_all_information_for_context(id: int):
+    return {"message": "Confirm person route"}
+
 
